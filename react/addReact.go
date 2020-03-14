720eb6e9df47d34b17a80236da6d6c9a04d0a0ef
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-03-14 14:43:53 +0800

    Finished React add functionality.
    - uses create-react-app (cra)
    - If the user doesn't have cra installed, canaveral will install it.
    
    TODO: finish tests for add react

--- react/addReact.go
+++ react/addReact.go
@@ -4,17 +4,54 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 )
+
+// checkCRAExists looks in the path for create-react-app
+// If there is no executable in the path, then throws error.
+// Else, returns a message with the path of create-react-app
+// ! untested
+func checkCRAExists() bool {
+	path, err := exec.LookPath("create-react-app")
+	if err != nil {
+		fmt.Printf("ERROR: didn't find 'create-react-app' in path\n")
+		return false
+	}
+	fmt.Printf("'create-react-app' executable is in '%s'\n", path)
+	return true
+}
+
+// installCRA() installs create-react-app.
+// REQUIRES (soft): create-react-app isn't already installed.
+// This is a soft requirement bc npm will just update it if it is.
+// ! untested
+func installCRA() {
+	fmt.Println(
+		"\nLooks like you don't have create-react-app. Let's install it...\n")
+	installCRA := exec.Command("npm", "install", "-g", "create-react-app")
+	installCRA.Stdout = os.Stdout
+	installCRA.Stderr = os.Stderr
+	err := installCRA.Run()
+	check(err)
+}
 
 // AddReactProj launches a new react project.
 // The main mechanism is similar to addProj (in root folder).
 // However, create-react-app plays a large role in setup.
+// ! untested
 func AddReactProj(projName string, wsPath string) {
 	ws, err := ioutil.ReadFile(wsPath)
 	check(err)
-	err = os.MkdirAll(string(ws)+"/"+projName, os.ModePerm)
+	err = os.Chdir(string(ws))
 	check(err)
-	err = os.Chdir(string(ws) + "/" + projName)
+	if !checkCRAExists() {
+		installCRA()
+	}
+	cmd := exec.Command("create-react-app", projName)
+	// set correct pipes
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
 	check(err)
 	fmt.Printf("Added React project %s to workspace %s\n", projName, string(ws))
 }
