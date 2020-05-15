60781575aa84fae45b6f2eb274f9143fc0afeffc
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-05-15 11:18:31 +0800

    Added more functions to addReactNative.
    Getting closer to full functionality...

--- reactnative/addReactNative.go
+++ reactnative/addReactNative.go
@@ -1,7 +1,12 @@
 package reactnative
 
 import (
+	"bufio"
+	"canaveral/lib"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 	"os/exec"
 )
 
@@ -31,11 +36,51 @@ func checkToolExists(toolName string) bool {
 }
 
 // ! Need to write expo install function
+func installExpo() error {
+	return nil
+}
+
+// confirmInstall listens for user confirmation and returns a boolean
+// Takes in an input channel to increase testability
+// ! untested
+func confirmInstall(stdin io.Reader) bool {
+	fmt.Printf("Allow canaveral to globally install 'expo'? ('y' or 'n')\n>")
+	reader := bufio.NewReader(stdin)
+	response, err := reader.ReadByte()
+	lib.Check(err)
+	return (response == 'y')
+}
 
 // AddReactNativeProj takes a project name and path, and inits an expo project.
 // First, must check if the user has installed expo or not.
 // If yes, then continue. If no, then ask to install.
 // Running 'expo init' with the project name will create a new project.
+// ! untested
 func AddReactNativeProj(projName string, wsPath string) {
-
+	expoInstalled := checkToolExists("expo")
+	ws, err := ioutil.ReadFile(wsPath)
+	lib.Check(err)
+	err = os.MkdirAll(string(ws), os.ModePerm)
+	lib.Check(err)
+	if !expoInstalled {
+		fmt.Println("Looks like you haven't installed 'expo'...")
+		confirm := confirmInstall(os.Stdin)
+		if !confirm {
+			fmt.Printf("Install rejected. Aborting creation of '%s'\n", projName)
+			return
+		}
+		err = installExpo()
+		lib.Check(err)
+	}
+	cmd := exec.Command("expo", "init", projName)
+	// set correct pipes
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// Go to canaveral workspace
+	err = os.Chdir(string(ws))
+	lib.Check(err)
+	// expo init projName
+	err = cmd.Run()
+	lib.Check(err)
+	fmt.Printf("Added React Native project %s to workspace %s\n", projName, string(ws))
 }
