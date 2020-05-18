0face789fb4b93bc753be46a8e7455803816fb56
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-05-18 11:13:58 +0800

    Initialized package structure for finder support.
    Need to add new command in main.go
    Need to write functionality for OpenFinder()
    Need to write tests

---
+++ finder/openFinder.go
@@ -0,0 +1,13 @@
+/*Package finder contains:
+	- functionality for opening canaveral project in finder
+// ? NOTE: this feature is targeted at macOS users
+*/
+package finder
+
+// OpenFinder will take a project name and config path,
+// and attempt to open the specified project in a new finder window.
+// Returns error if it fails. Otherwise returns nil.
+// ! untested
+func OpenFinder(projName string, configPath string) error {
+	return nil
+}
