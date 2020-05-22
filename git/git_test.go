74571c82a0230515974ea86e8b0b38b268d6700f
SeanPrendi <sean.prendi@gmail.com>
2020-05-22 18:28:09 -0400

    Changed all git tests to use different temp directories.

--- git/git_test.go
+++ git/git_test.go
@@ -15,7 +15,7 @@ func TestStatus(t *testing.T) {
 	usr, err := user.Current()
 	lib.Check(err)
 	home := usr.HomeDir
-	workingPath := home + "/canaveral_git_ws"
+	workingPath := home + "/canaveral_status_git_ws"
 	if _, err = os.Stat(workingPath); os.IsNotExist(err) {
 		os.MkdirAll(workingPath, os.ModePerm)
 		defer os.RemoveAll(workingPath)
@@ -45,7 +45,7 @@ func TestInit(t *testing.T) {
 	usr, err := user.Current()
 	lib.Check(err)
 	home := usr.HomeDir
-	workingPath := home + "/canaveral_git_ws"
+	workingPath := home + "/canaveral_init_git_ws"
 	if _, err = os.Stat(workingPath); os.IsNotExist(err) {
 		os.MkdirAll(workingPath, os.ModePerm)
 		defer os.RemoveAll(workingPath)
@@ -80,7 +80,7 @@ func TestAdd(t *testing.T) {
 	usr, err := user.Current()
 	lib.Check(err)
 	home := usr.HomeDir
-	workingPath := home + "/canaveral_git_ws"
+	workingPath := home + "/canaveral_add_git_ws"
 	if _, err = os.Stat(workingPath); os.IsNotExist(err) {
 		os.MkdirAll(workingPath, os.ModePerm)
 		defer os.RemoveAll(workingPath)
@@ -173,7 +173,7 @@ func TestCommit(t *testing.T) {
 	usr, err := user.Current()
 	lib.Check(err)
 	home := usr.HomeDir
-	workingPath := home + "/canaveral_git_ws"
+	workingPath := home + "/canaveral_commit_git_ws"
 	if _, err = os.Stat(workingPath); os.IsNotExist(err) {
 		os.MkdirAll(workingPath, os.ModePerm)
 		defer os.RemoveAll(workingPath)
@@ -290,7 +290,7 @@ func TestIgnore(t *testing.T) {
 	usr, err := user.Current()
 	lib.Check(err)
 	home := usr.HomeDir
-	workingPath := home + "/canaveral_git_ws"
+	workingPath := home + "/canaveral_ignore_git_ws"
 	if _, err = os.Stat(workingPath); os.IsNotExist(err) {
 		os.MkdirAll(workingPath, os.ModePerm)
 		defer os.RemoveAll(workingPath)
